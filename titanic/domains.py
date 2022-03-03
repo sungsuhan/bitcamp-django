fa4c2d79932e789ec701e8bf0d27238b3f31998a
sungsuhan <swhanssu@gmail.com>
2022-03-03 16:44:06 +0900

    2022-03-03 16:43 타이타닉

---
+++ titanic/domains.py
@@ -0,0 +1,41 @@
+# context, fname, train, test, id, lable
+from dataclasses import dataclass
+
+@dataclass
+class Dataset:
+    context : str
+    fname : str
+    train : str
+    test : str
+    id : str
+    lable : str
+
+    @property
+    def context(self) -> str: return self._context
+    @context.setter
+    def context(self, context): self._context = context
+
+    @property
+    def fname(self) -> str: return self._fname
+    @fname.setter
+    def fname(self, fname): self._fname = fname
+
+    @property
+    def train(self) -> str: return self._train
+    @train.setter
+    def train(self, train): self._train = train
+
+    @property
+    def test(self) -> str: return self._test
+    @test.setter
+    def test(self, test): self._test = test
+
+    @property
+    def id(self) -> str: return self._id
+    @id.setter
+    def id(self, id): self._id = id
+
+    @property
+    def lable(self) -> str: return self._lable
+    @lable.setter
+    def lable(self, lable): self._lable = lable
